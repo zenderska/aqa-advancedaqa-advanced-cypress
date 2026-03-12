32ceba2f020f9f6717557b6667807861589320ae
Viktor H <zenderska@gmail.com>
2026-03-12 18:23:55 +0200

    HM18_install_cypress_and_edit_config

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  
+    "defaultCommandTimeout": 5000
+  },
+});
