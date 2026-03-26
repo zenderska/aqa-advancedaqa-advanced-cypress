2d4ce0177304358cae0848ec34cd96643a4368a5
Viktor H <zenderska@gmail.com>
2026-03-26 14:46:02 +0200

    HM21_add_new_cypress_test1

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // можеш додати node events тут, якщо потрібно
       return config;
     },
-    baseUrl: "/", // базовий URL можна залишити пустим, бо у нас різні домени
+    baseUrl: "/",
   },
   env: {
     usersFile: "users.json"
